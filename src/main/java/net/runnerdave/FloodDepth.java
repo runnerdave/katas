b1e5d8aed9aeafddaf111fd4cec54c44281f29d1
David Jimenez <david.jimenez@rmit.edu.au>
2016-10-14 15:12:19 +1100

    solved the algorithm

--- src/main/java/net/runnerdave/FloodDepth.java
+++ src/main/java/net/runnerdave/FloodDepth.java
@@ -1,36 +1,35 @@
 package net.runnerdave;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
- * Created by davidajimenez on 13/10/2016.
- * solution for this problem:
- * https://codility.com/programmers/lessons/90-tasks_from_indeed_prime_2015_challenge/flood_depth/
+ * Created by davidajimenez on 13/10/2016. solution for this problem:
+ * https://codility.com/programmers/lessons/90-
+ * tasks_from_indeed_prime_2015_challenge/flood_depth/
  */
 public class FloodDepth {
-    Map<Integer, Container> containerMap = new HashMap<>();
 
-    //returns maximum depth of water
-    public static int solution(int[] A){
-        int maxDepth = 0;
-        int edge = A[0];
-        for (int i = 0; i < (A.length - 1); i++) {
-            int depth = 0;
-            while(A[i]>A[i+1]) {
-                depth = A[i] - A[i+1];
-            }
-        }
-        return maxDepth;
-    }
-
-    private class Container {
-        int maxDepth;
-        int width;
-
-        Container(int maxDepth, int width) {
-            this.maxDepth = maxDepth;
-            this.width = width;
-        }
-    }
+	// returns maximum depth of water
+	public static int solution(int[] A) {
+		int maxDepth = 0;
+		int firstEdge = A[0];
+		int secondEdge = 0;
+		int depth = 0;
+		for (int i = 0; i < (A.length - 1); i++) {
+			if (A[i] > A[i + 1]) {
+				firstEdge = A[i];
+				depth = A[i] - A[i + 1];
+				if (maxDepth < depth) {
+					maxDepth = depth;
+				}
+			} else if (A[i + 1] >= firstEdge) {
+				secondEdge = A[i + 1];
+			} else if (i == (A.length - 2) && A[i + 1] < firstEdge) {
+				secondEdge = A[i + 1];
+				maxDepth = secondEdge - A[i];
+			}
+		}
+		if (secondEdge == 0) {
+			maxDepth = 0;
+		}
+		return maxDepth;
+	}
 }
--- src/test/java/net/runnerdave/FloodDepthTest.java
+++ src/test/java/net/runnerdave/FloodDepthTest.java
@@ -1,18 +1,40 @@
 package net.runnerdave;
 
-import static org.junit.Assert.*;
-import java.math.BigInteger;
-import java.util.Random;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 /**
  * Created by davidajimenez on 13/10/2016.
  */
 public class FloodDepthTest {
-    @Test
-    public void test0() {
-        assertEquals(BigInteger.valueOf(28), FloodDepth.solution(new int[]{1,3,2,1,2,1,5,3,3,4,2}));
-    }
+	@Test
+	public void test0() {
+		assertEquals(2, FloodDepth.solution(new int[] { 1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2 }));
+	}
 
-
+	@Test
+	public void test1() {
+		assertEquals(3, FloodDepth.solution(new int[] { 4, 1, 4 }));
+	}
+	
+	@Test
+	public void test2() {
+		assertEquals(0, FloodDepth.solution(new int[] { 2, 1 }));
+	}
+	
+	@Test
+	public void test3() {
+		assertEquals(0, FloodDepth.solution(new int[] { 1, 2 }));
+	}
+	
+	@Test
+	public void test4() {
+		assertEquals(0, FloodDepth.solution(new int[] { 3, 2, 1 }));
+	}
+	
+	@Test
+	public void test5() {
+		assertEquals(1, FloodDepth.solution(new int[] { 3, 1, 2 }));
+	}
 }
