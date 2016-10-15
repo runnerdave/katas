4f0611f046fd065384513042e0e28b1030d9b366
runnerdave <runnerdave@gmail.com>
2016-10-15 15:37:26 +1100

    flood depth finished for a 66 mark

--- src/main/java/net/runnerdave/FloodDepth.java
+++ src/main/java/net/runnerdave/FloodDepth.java
@@ -1,5 +1,8 @@
 package net.runnerdave;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by davidajimenez on 13/10/2016. solution for this problem:
  * https://codility.com/programmers/lessons/90-
@@ -27,12 +30,224 @@ public class FloodDepth {
             }
         }
 
-        //run in reverse and pick the smallest of the two maxDepths
+        if (secondEdge == 0) {
+            maxDepth = 0;
+        }
 
+        //run in reverse and pick the smallest of the two maxDepths
+        int reverseMaxDepth = solutionArrayInReverse(A);
+
+        //return maxDepth;
+        return maxDepth < reverseMaxDepth ? reverseMaxDepth : maxDepth;
+    }
+
+    public static int solutionArrayInReverse(int[] A) {
+        int maxDepth = 0;
+        int firstEdge = A[0];
+        int secondEdge = 0;
+        int depth = 0;
+
+
+        for (int i = (A.length - 1); i > 0; i--) {
+            if (A[i - 1] < A[i]) {
+                firstEdge = A[i];
+                depth = A[i] - A[i - 1];
+                if (maxDepth < depth) {
+                    maxDepth = depth;
+                }
+            } else if (A[i - 1] >= firstEdge) {
+                secondEdge = A[i - 1];
+            }
+        }
 
         if (secondEdge == 0) {
             maxDepth = 0;
         }
         return maxDepth;
     }
+
+
+
+    public static int solutionWithEdges(int[] A) {
+        int maxDepth = 0;
+        int firstEdge = A[0];
+        int secondEdge = 0;
+        int depth = 0;
+        boolean inContainer = false;
+        int containerMaxDepth = 0;
+
+        for (int i = 0; i < (A.length - 1); i++) {
+
+            if (A[i] > A[i + 1]) {
+
+                if (!inContainer) {
+                    firstEdge = A[i];
+                    inContainer = true;
+                }
+                depth = firstEdge - A[i + 1];
+
+                if (containerMaxDepth < depth) {
+                    containerMaxDepth = depth;
+                }
+
+            } else if (inContainer && A[i + 1] >= firstEdge) {
+                //solves 3,2,3 and 3,2,4 but fails at 4,2,3,4
+                secondEdge = A[i + 1];
+                inContainer = false;
+                if (maxDepth < containerMaxDepth) {
+                    maxDepth = containerMaxDepth;
+                }
+                containerMaxDepth = 0;
+            } else if (inContainer && (i+2) < A.length ) {
+                //run a while until i
+
+
+                if(A[i+2] < A[i+1]) {
+                    secondEdge = A[i + 1];
+                    inContainer = false;
+                    containerMaxDepth = containerMaxDepth - (firstEdge - secondEdge);
+                    if (maxDepth < containerMaxDepth) {
+                        maxDepth = containerMaxDepth;
+                    }
+                    containerMaxDepth = 0;
+                }
+            } else if (inContainer && (i+2) == A.length ) {
+                if(A[i] < A[i+1]) {
+                    secondEdge = A[i + 1];
+                    inContainer = false;
+                    containerMaxDepth = containerMaxDepth - (firstEdge - secondEdge);
+                    if (maxDepth < containerMaxDepth) {
+                        maxDepth = containerMaxDepth;
+                    }
+                    containerMaxDepth = 0;
+                }
+            }
+        }
+
+        if (secondEdge == 0) {
+            maxDepth = 0;
+        }
+
+
+        //return maxDepth;
+        return maxDepth;
+    }
+
+    public static int solutionWithEdges2(int[] A) {
+        int maxDepth = 0;
+        int firstEdge = A[0];
+        int secondEdge = 0;
+        int depth = 0;
+        boolean inContainer = false;
+        int containerMaxDepth = 0;
+
+        for (int i = 0; i < (A.length - 1); i++) {
+
+            if (A[i] > A[i + 1]) {
+
+                if (!inContainer) {
+                    firstEdge = A[i];
+                    inContainer = true;
+                }
+                depth = firstEdge - A[i + 1];
+
+                if (containerMaxDepth < depth) {
+                    containerMaxDepth = depth;
+                }
+
+            } else if (inContainer && A[i + 1] >= firstEdge) {
+                //solves 3,2,3 and 3,2,4 but fails at 4,2,3,4
+                secondEdge = A[i + 1];
+                inContainer = false;
+                if (maxDepth < containerMaxDepth) {
+                    maxDepth = containerMaxDepth;
+                }
+                containerMaxDepth = 0;
+            } else if (inContainer && A[i + 1] < firstEdge) {
+                //check if second last if so stop here
+                if (inContainer && (i+2) == A.length ) {
+                    if (A[i] < A[i + 1]) {
+                        secondEdge = A[i + 1];
+                        inContainer = false;
+                        containerMaxDepth = containerMaxDepth - (firstEdge - secondEdge);
+                        if (maxDepth < containerMaxDepth) {
+                            maxDepth = containerMaxDepth;
+                        }
+                        containerMaxDepth = 0;
+                    }
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        if (secondEdge == 0) {
+            maxDepth = 0;
+        }
+
+        return maxDepth;
+    }
+
+    public static int solutionWithEdges3(int[] A) {
+        int maxDepth = 0;
+        int firstEdge = A[0];
+        int secondEdge = 0;
+        int depth = 0;
+        boolean inContainer = false;
+        int containerMaxDepth = 0;
+
+        for (int i = 0; i < (A.length - 1); i++) {
+
+            if (A[i] > A[i + 1]) {
+
+                if (!inContainer) {
+                    firstEdge = A[i];
+                    inContainer = true;
+                }
+                depth = firstEdge - A[i + 1];
+
+                if (containerMaxDepth < depth) {
+                    containerMaxDepth = depth;
+                }
+
+            } else if (inContainer && A[i + 1] >= firstEdge) {
+                //solves 3,2,3 and 3,2,4 but fails at 4,2,3,4
+                secondEdge = A[i + 1];
+                inContainer = false;
+                if (maxDepth < containerMaxDepth) {
+                    maxDepth = containerMaxDepth;
+                }
+                containerMaxDepth = 0;
+            } else if (inContainer && A[i + 1] < firstEdge) {
+                //check if second last if so stop here
+                if ((i+2) == A.length ) {
+                    if (A[i] < A[i + 1]) {
+                        secondEdge = A[i + 1];
+                        inContainer = false;
+                        containerMaxDepth = containerMaxDepth - (firstEdge - secondEdge);
+                        if (maxDepth < containerMaxDepth) {
+                            maxDepth = containerMaxDepth;
+                        }
+                        containerMaxDepth = 0;
+                    }
+                } else {
+                    if (A[i] < A[i + 1]) {
+                        secondEdge = A[i + 1];
+                        inContainer = false;
+                        containerMaxDepth = containerMaxDepth - (firstEdge - secondEdge);
+                        if (maxDepth < containerMaxDepth) {
+                            maxDepth = containerMaxDepth;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (secondEdge == 0) {
+            maxDepth = 0;
+        }
+
+        return maxDepth;
+    }
+
 }
--- src/test/java/net/runnerdave/FloodDepthTest.java
+++ src/test/java/net/runnerdave/FloodDepthTest.java
@@ -8,48 +8,179 @@ import org.junit.Test;
  * Created by davidajimenez on 13/10/2016.
  */
 public class FloodDepthTest {
-	@Test
-	public void test0() {
-		assertEquals(2, FloodDepth.solution(new int[] { 1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2 }));
-	}
 
-	@Test
-	public void test1() {
-		assertEquals(3, FloodDepth.solution(new int[] { 4, 1, 4 }));
-	}
-	
-	@Test
-	public void test2() {
-		assertEquals(0, FloodDepth.solution(new int[] { 2, 1 }));
-	}
-	
-	@Test
-	public void test3() {
-		assertEquals(0, FloodDepth.solution(new int[] { 1, 2 }));
-	}
-	
-	@Test
-	public void test4() {
-		assertEquals(0, FloodDepth.solution(new int[] { 3, 2, 1 }));
-	}
-	
-	@Test
-	public void test5() {
-		assertEquals(1, FloodDepth.solution(new int[] { 3, 1, 2 }));
-	}
 
-	@Test
-	public void test6() {
-		assertEquals(2, FloodDepth.solution(new int[] { 3, 1, 4, 2, 5, 1, 2 }));
-	}
+    @Test
+    public void test0_2() {
+        assertEquals(2, FloodDepth.solutionWithEdges(new int[]{1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2}));
+    }
+
+    @Test
+    public void test0_3() {
+        assertEquals(2, FloodDepth.solutionWithEdges2(new int[]{1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2}));
+    }
+
+    @Test
+    public void test0_4() {
+        assertEquals(2, FloodDepth.solutionWithEdges3(new int[]{1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2}));
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(3, FloodDepth.solution(new int[]{4, 1, 4}));
+    }
+
+    @Test
+    public void test1_1() {
+        assertEquals(3, FloodDepth.solutionArrayInReverse(new int[]{4, 1, 4}));
+    }
+
+    @Test
+    public void test1_2() {
+        assertEquals(3, FloodDepth.solutionWithEdges(new int[]{4, 1, 4}));
+    }
+
+    @Test
+    public void test1_3() {
+        assertEquals(3, FloodDepth.solutionWithEdges2(new int[]{4, 1, 4}));
+    }
+
+
+    @Test
+    public void test2() {
+        assertEquals(0, FloodDepth.solution(new int[]{2, 1}));
+    }
+
+    @Test
+    public void test2_3() {
+        assertEquals(0, FloodDepth.solutionWithEdges2(new int[]{2, 1}));
+    }
+
+
+    @Test
+    public void test3() {
+        assertEquals(0, FloodDepth.solution(new int[]{1, 2}));
+    }
+
+    @Test
+    public void test3_3() {
+        assertEquals(0, FloodDepth.solutionWithEdges2(new int[]{1, 2}));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(0, FloodDepth.solution(new int[]{3, 2, 1}));
+    }
+
+    @Test
+    public void test4_3() {
+        assertEquals(0, FloodDepth.solutionWithEdges2(new int[]{3, 2, 1}));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(1, FloodDepth.solution(new int[]{3, 1, 2}));
+    }
+
+    @Test
+    public void test5_1() {
+        assertEquals(1, FloodDepth.solutionArrayInReverse(new int[]{3, 1, 2}));
+    }
+
+    @Test
+    public void test5_3() {
+        assertEquals(1, FloodDepth.solutionWithEdges2(new int[]{3, 1, 2}));
+    }
+
+
+    @Test
+    public void test6_0() {
+        assertEquals(2, FloodDepth.solutionWithEdges(new int[]{3, 1, 4, 2, 5, 1, 2}));
+    }
+
+    @Test
+    public void test6_3() {
+        assertEquals(2, FloodDepth.solutionWithEdges2(new int[]{3, 1, 4, 2, 5, 1, 2}));
+    }
+
+    @Test
+    public void test6_4() {
+        assertEquals(2, FloodDepth.solutionWithEdges3(new int[]{3, 1, 4, 2, 5, 1, 2}));
+    }
+
+    @Test
+    public void test6_1() {
+        assertEquals(0, FloodDepth.solutionArrayInReverse(new int[]{1, 2, 3}));
+    }
 
     @Test
     public void test7() {
-        assertEquals(1, FloodDepth.solution(new int[] { 3, 2, 4}));
+        assertEquals(1, FloodDepth.solution(new int[]{3, 2, 4}));
+    }
+
+    @Test
+    public void test7_3() {
+        assertEquals(1, FloodDepth.solutionWithEdges2(new int[]{3, 2, 4}));
     }
 
     @Test
     public void test8() {
-        assertEquals(0, FloodDepth.solution(new int[] { 1, 2, 3 }));
+        assertEquals(0, FloodDepth.solution(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void test8_1() {
+        assertEquals(0, FloodDepth.solutionArrayInReverse(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void test8_3() {
+        assertEquals(0, FloodDepth.solutionWithEdges2(new int[]{1, 2, 3}));
+    }
+
+
+    @Test
+    public void test9_0() {
+        assertEquals(2, FloodDepth.solutionWithEdges(new int[]{2, 1, 4, 1, 2, 2, 3}));
+    }
+
+    @Test
+    public void test9_3() {
+        assertEquals(2, FloodDepth.solutionWithEdges2(new int[]{2, 1, 4, 1, 2, 2, 3}));
+    }
+
+    @Test
+    public void test10_0() {
+        assertEquals(3, FloodDepth.solutionWithEdges(new int[]{4, 1, 1, 3, 1, 1, 5, 1, 1}));
+    }
+
+    @Test
+    public void test10_3() {
+        assertEquals(3, FloodDepth.solutionWithEdges2(new int[]{4, 1, 1, 3, 1, 1, 5, 1, 1}));
+    }
+
+    @Test
+    public void test10_4() {
+        assertEquals(3, FloodDepth.solutionWithEdges3(new int[]{4, 1, 1, 3, 1, 1, 5, 1, 1}));
+    }
+
+    @Test
+    public void test11_0() {
+        assertEquals(2, FloodDepth.solutionWithEdges(new int[]{4, 1, 1, 3, 1, 1, 3}));
+    }
+
+    @Test
+    public void test11_3() {
+        assertEquals(2, FloodDepth.solutionWithEdges2(new int[]{4, 1, 1, 3, 1, 1, 3}));
+    }
+
+    @Test
+    public void test11_4() {
+        assertEquals(2, FloodDepth.solutionWithEdges3(new int[]{4, 1, 1, 3, 1, 1, 3}));
+    }
+
+    @Test
+    public void test12() {
+        assertEquals(6, FloodDepth.solutionWithEdges3(new int[]{20, 4, 10, 1, 3}));
     }
 }
