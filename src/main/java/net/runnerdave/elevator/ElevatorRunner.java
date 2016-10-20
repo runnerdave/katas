d190df82351480c8aef62e4566f8032e053357c5
David Jimenez <david.jimenez@rmit.edu.au>
2016-10-20 16:34:52 +1100

    work on elevator and memory examples

--- src/main/java/net/runnerdave/elevator/ElevatorRunner.java
+++ src/main/java/net/runnerdave/elevator/ElevatorRunner.java
@@ -10,12 +10,29 @@ public class ElevatorRunner {
         System.out.println("Welcome to 440 Elizabeth st. Melbourne");
         ElevatorController ec = new ElevatorController(true);
         int totalStops = 0;
-        int totalRuns = 200;
+        int totalRuns = 150;
         while (totalRuns > 0) {
             System.out.println("run : " + totalRuns);
             totalStops = ec.transportAllPeople(new int[]{40, 40, 100, 80, 20}, new int[]{3, 3, 2, 2, 3}, 3, 5, 200);
             System.out.println("total number of stops: " + totalStops);
             Thread.sleep(1000); // 1 SECOND
+            Object[] objects;
+            if (totalRuns == 150) {
+                objects = new Object[10000000];
+            }
+
+            if (totalRuns == 100) {
+                objects = null;
+            }
+
+            if (totalRuns == 90) {
+                Integer count = 0;
+                for(long x=0;x<Integer.MAX_VALUE ;x++){
+                    count +=1;
+                }
+                System.out.println("count: " + count);
+            }
+
             totalRuns--;
         }
     }
